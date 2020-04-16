bb48ffa3f74eddb4f5005e0d4266bf5e4b0e1d94
Christopher Shaw <chris@chris-shaw.com>
2020-04-16 13:00:13 +0100

    Update build.php

--- build.php
+++ build.php
@@ -69,7 +69,7 @@ document.getElementById("search").addEventListener("keyup", function(){
    	let current = searchable[i]; 
 	let title = current.querySelectorAll("a")[0].innerHTML; 
     	let similarity = string_similarity(title, this.value);
-    if(similarity > 0.05) {
+    if(similarity > 0.8) {
       current.classList.remove('hide');
 
     }
